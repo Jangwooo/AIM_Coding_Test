5fcd6c66f347d178b6d20896f26d8a56884c27bf
Jangwoo <kjw2262@gmail.com>
2024-05-06 09:24:55 +0900

    사용하지 않는 에러 제거

--- pkg/error.go
+++ pkg/error.go
@@ -5,7 +5,6 @@ import (
 )
 
 var (
-	ErrUserNotFound             = errors.New("user not found")
 	ErrPasswordNotMatch         = errors.New("password not match")
 	ErrDuplicateUser            = errors.New("duplicate user")
 	ErrDatabaseConnectionFailed = errors.New("database connection failed")
@@ -13,7 +12,6 @@ var (
 	ErrSessionExpired           = errors.New("session expired. please login again")
 	ErrForbiddenResourceAccess  = errors.New("forbidden to access this resource")
 	ErrOnlyAdminCanAccess       = errors.New("only admin can access this resource")
-	ErrDuplicateAccountID       = errors.New("duplicate account id")
 	ErrRecordNotFound           = errors.New("record not found")
 	ErrBalanceNotEnough         = errors.New("balance not enough")
 	ErrDuplicateStockID         = errors.New("duplicate stock id")
